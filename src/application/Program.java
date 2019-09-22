557fbd0453b4e0ad5ca9b6459d17e2b1f3d087f7
Edson Alexandre <edson.alexandre@outlook.com>
2019-09-22 08:49:37 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,8 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -34,7 +37,23 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		
+		System.out.println("\n==== TEST 4 : seller Insert ====");
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Seller newSeller;
+		try {
+			newSeller = new Seller(0,"Funalo","fulano@gmail.com",new Date(sdf.parse("13/05/1981").getTime()),3000.0,department);
+			sellerDao.insert(newSeller);
+			System.out.println("Inserted! new Id: "+newSeller.getId());
+		} catch (ParseException e) {
+			e.printStackTrace();
+		} 
+
+		System.out.println("\n==== TEST 5 : seller Update ====");
+		seller = sellerDao.findByID(9);
+		seller.setName("Eduardo Alegre");
+		sellerDao.update(seller);
+		seller = sellerDao.findByID(9);
+		System.out.println("Update completed! new Seller:"+seller);
 	}
 
 }
